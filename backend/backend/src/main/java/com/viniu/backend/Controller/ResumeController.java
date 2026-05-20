bb45b8a118bf404545049e3be4beedfd9b45adf5
Foretiner <vdatta182@gmail.com>
2026-05-20 14:45:30 +0000

    Fixed port issue

--- backend/backend/src/main/java/com/viniu/backend/Controller/ResumeController.java
+++ backend/backend/src/main/java/com/viniu/backend/Controller/ResumeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://ai-resume-analyzer-three-iota.vercel.app/")
 @RequestMapping("/api/resume")
 public class ResumeController {
 
