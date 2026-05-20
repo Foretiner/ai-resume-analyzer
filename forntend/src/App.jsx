adaa6d3234c628440c1e2b915462846cb6927711
Foretiner <vdatta182@gmail.com>
2026-05-20 14:28:54 +0000

    Fixed HTTPS issue

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
 
     try {
       const response = await fetch(
-        "http://ai-resume-analyzer-production-793c.up.railway.app/api/resume/upload",
+        "https://ai-resume-analyzer-production-793c.up.railway.app/api/resume/upload",
         {
           method: "POST",
           body: formData,
