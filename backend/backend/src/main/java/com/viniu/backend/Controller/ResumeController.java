b45ceca36822965007347455eef812b00f55171b
Foretiner <vdatta182@gmail.com>
2026-05-20 14:17:29 +0000

    Fixed CROS issue

--- backend/backend/src/main/java/com/viniu/backend/Controller/ResumeController.java
+++ backend/backend/src/main/java/com/viniu/backend/Controller/ResumeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/resume")
 @CrossOrigin(origins = "*")
+@RequestMapping("/api/resume")
 public class ResumeController {
 
     @Autowired
